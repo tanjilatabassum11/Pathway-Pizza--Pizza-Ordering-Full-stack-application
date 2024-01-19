49118b6aa0cef543f6c1f438b45d6895b095cfdb
Destin Bernie <berniedestin7@gmail.com>
2024-01-19 09:49:43 -0500

    Finally got the User Pizza Form to work with adding and removing toppings

--- vue/src/services/PizzaService.js
+++ vue/src/services/PizzaService.js
@@ -4,5 +4,17 @@ export default{
 
     getAllSpecialtyPizzas(){
         return axios.get('/pizzas/specialty');
+    },
+    addToppingToPizza(pizzaId, toppingId){
+        return axios.post(`/pizzas/${pizzaId}/add/${toppingId}`);
+    },
+    removeToppingFromPizza(pizzaId, toppingId){
+        return axios.delete(`/pizzas/${pizzaId}/remove/${toppingId}`);
+    },
+    updatePizza(pizza){
+        return axios.put(`/pizzas/update`, pizza);
+    },
+    createSpecialtyPizza(pizza){
+        return axios.post(`/pizzas/specialty`, pizza);
     }
 }
