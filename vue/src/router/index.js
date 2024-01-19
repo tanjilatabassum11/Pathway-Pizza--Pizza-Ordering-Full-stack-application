34fbef5246993863661a17dfbc8377f233476144
Destin Bernie <berniedestin7@gmail.com>
2024-01-19 16:22:05 -0500

    Fix router, website now works

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import { useStore } from 'vuex';
+import { createRouter as createRouter, createWebHistory } from 'vue-router'
+import { useStore } from 'vuex'
 
+// Import components
 import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
 import LogoutView from '../views/LogoutView.vue';
@@ -12,6 +13,14 @@ import UserToppingsView from '../views/UserToppingsView.vue';
 import OrderConfirmationView from '../views/OrderConfirmationView.vue';
 import UserOrdersView from '../views/UserOrdersView.vue';
 
+/**
+ * The Vue Router is used to "direct" the browser to render a specific view component
+ * inside of App.vue depending on the URL.
+ *
+ * It also is used to detect whether or not a route requires the user to have first authenticated.
+ * If the user has not yet authenticated (and needs to) they are redirected to /login
+ * If they have (or don't need to) they're allowed to go about their way.
+ */
 const routes = [
   {
     path: '/',
@@ -95,19 +104,32 @@ const routes = [
   },
 ];
 
+// Create the router
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes: routes
 });
 
-router.beforeEach((to, from, next) => {
-  const store = useStore();
-  const requiresAuth = to.matched.some(route => route.meta.requiresAuth);
+router.beforeEach((to) => {
 
-  if (requiresAuth && !store.getters.isAuthenticated) {
-    next({ name: 'login' });
-  } else {
-    next();
+  // Get the Vuex store
+  const store = useStore();
+  // Determine if the route requires Authentication
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+
+  // If it does and they are not logged in, send the user to "/login"
+  if (requiresAuth && store.state.token === '') {
+    return {name: "login"};
+
+
+// router.beforeEach((to, from, next) => {
+//   const store = useStore();
+//   const requiresAuth = to.matched.some(route => route.meta.requiresAuth);
+
+//   if (requiresAuth && !store.getters.isAuthenticated) {
+//     next({ name: 'login' });
+//   } else {
+//     next();
   }
 });
 
