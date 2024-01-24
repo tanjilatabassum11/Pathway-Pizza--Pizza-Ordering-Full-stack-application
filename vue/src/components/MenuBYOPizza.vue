f37eb6a8d462640cfdc9b80a9eedc8f8600b85f8
Bailey <baileymaben1@gmail.com>
2024-01-23 20:42:07 -0500

    Messed around with Menu BYOP styles

--- vue/src/components/MenuBYOPizza.vue
+++ vue/src/components/MenuBYOPizza.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="topping-card">
+        <div class="header">
   <h2 id="BYOP">Build Your Own Pizza!</h2>
-  <div id="container">
+        </div>
+  <div class="container" id="container">
     <div class="topping-style">
         <h3 
             class="Crust type"> Crust Style
@@ -38,17 +40,25 @@
         <h3 
             class="Fruit type"> Fruit Toppings
         </h3>
+        
         <div class="toppings"
             v-for="topping in fruitToppings" :key="topping.topping_id">{{ topping.topping_name }} - ${{ topping.cost }}
         </div>
+        
+        <br/>
+        <br/>
+        <br/>
     </div>
     <div class="topping-style">
         <h3 
             class="Veggie type"> Veggie Toppings
         </h3>
+        
         <div class="toppings"
-            v-for="topping in veggieToppings" :key="topping.topping_id">{{ topping.topping_name }} - ${{ topping.cost }}
+            v-for="topping in veggieToppings" :key="topping.topping_id">{{ topping.topping_name }} - ${{ topping.cost }} 
         </div>
+        
+      
     </div>
   </div>
 </div>
@@ -117,102 +127,72 @@ export default {
     font-style: normal;
     font-display: swap;
 }
-.topping-card{
+.topping-card {
     box-sizing: border-box;
     background-color: #a18f6380;
 }
-.type{
-    font-family: "Cooper Hewitt Bold", sans-serif;
+.container {
+    width: 100%;
+    height: 50vh;
+    display: flex;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: baseline;
+    align-content: center;
+    row-gap: 1rem;
+    column-gap: 1rem;
+    flex-grow: 1;
+    flex-shrink: 0;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+#container > * {
+    width: 25%;
+    flex-grow: 1;
+}
+    
+.topping-style{
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0,0,0, .01);
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    box-sizing: border-box;
+    border: 2px inset #BB554A;
+}
+.toppings{
+   margin: auto;
+   padding: 5px 1px;
+    text-transform: uppercase;
+}
+#BYOP {
+    font-family: 'Mandalore Laser Title';
+    color: #BB554A;
+    background: linear-gradient(45deg, #a18f6380 50%, white 100%);
+    height: 100%;
+}
+
+.type {
+    font-family: 'Mandalore Laser Title';
     text-decoration: underline;
     color: white;
-    padding: 4px;
-    border-bottom: 1px solid black;
+    padding: 14px;
+    border-bottom: 1px inset black;
+    margin: 5px;
 }
 .toppings:not(:last-child){
     border-bottom: 1px solid black;
 }
-.toppings:last-child selector{
-    border-bottom: none;
-}
-div{
-    
-}
-h2{
-    font-family: 'Mandalore Laser Title';
-    color: #BB554A;
-}
-#BYOP{
-    font-family: 'Mandalore Laser Title';
-    color: #BB554A;
-    background: linear-gradient(45deg, #a18f6380 50%, white 100%)
-;
-}
-#container{
-    /* display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    flex-wrap: wrap; */
-}
-.topping-style{
-    /* margin: auto; */
-    padding-top: 5px;
-    padding-bottom: 5px;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, .9999999999999999999999);
-    /* display: flex; */
-    /* flex-direction: column;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    align-items: flex-start; */
-    width: 100px;
-    text-transform: uppercase;
-    text-align: left;
-}
-.Crust{
-    /* background-color: #A18F63; */
-    box-sizing: border-box;
+
+.type {
     background-color: #a18f6380;
     color: #BB554A;
-    border: #5FA873 2px solid;
+    border: #5FA873 2px inset;
     border-radius: 5px;
-    transition: 250ms;
 }
-.Sauce{
-    background-color: #BB554A;
-}
-.Cheese{
-    background-color: #5FA873;
-}
-.Meat{
-    background-color: #A18F63;
-}
-.Fruit{
-    background-color: #BB554A;
-}
-.Veggie{
-    background-color: #5FA873;
-}
-@media only screen and (max-width: 678px) {
-    #container{
-        flex-direction: column;
-        flex-wrap: nowrap;
-        padding-left: 25%;
-        padding-right: 25%;
-    }
-}
-@media only screen and (max-width: 767px) {
-    #container{
-        flex-direction: row;
-        flex-wrap: wrap;
-    }
-}
-#container{
-    display: flex;
-    flex-wrap: wrap;
-}
-.toppings-style{
-    flex: 1 0 30%;
-    margin: 10px;
-    box-sizing: border-box;
-}
+
+
+
 </style>
