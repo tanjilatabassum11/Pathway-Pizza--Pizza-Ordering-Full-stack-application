f2c0174464b60f59120588d74eb89f350789a5d2
Gene Jackson II <genejacksonii1993@gmail.com>
2024-01-25 10:01:52 -0500

    Cleaned up view files, removed unnecessary code/comments

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="capstone-app">
-    <!-- <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div> -->
+
     <LogoComponent />
     
     <router-view />
